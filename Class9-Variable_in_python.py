0568d684527d8b870e8facbdbd1b5facde4e97f7
Yash Sindhu <yashsindhu123@gmail.com>
2022-11-27 23:32:20 +0530

    Add files via upload

---
+++ Class9-Variable_in_python.py
@@ -0,0 +1,13 @@
+number1 = 2
+print(number1)
+number1 = 4
+print(number1)
+name = "Yash"
+print(name)
+name = 123
+print(name)
+
+# Rule no.1
+# 1number = 4  --> gives error
+# letter first word should be capital
+_namw = "Yash" # gives error
