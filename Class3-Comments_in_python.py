dd8730718cd4bb4f2111fa04f5f103e7c6a8b978
Yash Sindhu <yashsindhu123@gmail.com>
2022-11-27 20:18:57 +0530

    Add files via upload

---
+++ Class3-Comments_in_python.py
@@ -0,0 +1,9 @@
+print("line A \nline B \nline C")
+# \n is for new line
+print("name\t Yash")
+# \t is for tab
+print("this is \\\\ double backslash")
+# \\ is for backslash
+print("hell\blo")
+# \b is for backspace
+# "#" is added in starting of a comment
