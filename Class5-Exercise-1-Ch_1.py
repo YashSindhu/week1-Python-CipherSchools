f34a2ad5b930a6c45c4548a58be7f57d373b5bc3
Yash Sindhu <yashsindhu123@gmail.com>
2022-11-27 20:40:59 +0530

    Add files via upload

---
+++ Class5-Exercise-1-Ch_1.py
@@ -0,0 +1,12 @@
+# ************ print these following lines **********************
+# this is \\ double backslash
+# this is /\/\/\/\/\ mountain
+# he is      awesome (use escape sequences instead of manual spaces)
+# \" \n \t \' (print this as an output )
+
+# SOLUTIONS:-
+
+print("this is \\\\ double backslash")
+print("this is /\\/\\/\\/\\/\\ mountains")
+print("he is\t awesome")
+print("\\\" \\n \\t \\\' ")
