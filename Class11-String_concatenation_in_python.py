e112099db9f5af79af176ac3fe4f17b4c4af6e61
Yash Sindhu <yashsindhu123@gmail.com>
2022-11-28 23:57:54 +0530

    Add files via upload

---
+++ Class11-String_concatenation_in_python.py
@@ -0,0 +1,10 @@
+# srings
+# collection of characters inside single quotes or double quotes
+
+first_name = "Yash"
+last_name = "Sindhu"
+full_name = first_name+" "+last_name
+print(full_name)
+# print(first_name + 3) --> gives error
+# print(first_name + str(3)) --> no error
+# print(first_name * 3) --> no error
