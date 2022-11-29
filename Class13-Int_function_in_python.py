210bebd243e5fa31df23eb7a089b27108951123b
Yash Sindhu <yashsindhu123@gmail.com>
2022-11-29 13:55:48 +0530

    Add files via upload

---
+++ Class13-Int_function_in_python.py
@@ -0,0 +1,12 @@
+number_one = input("enter first number")
+number_two = input("enter second number")
+total = number_one+number_two
+print("total is" + total)
+# total = "4" +"4" =44 
+
+
+num_1 =str(4)
+num_2 =float("44")
+num_3 = int("33")
+print(num_2 +num_3)
+# its output is 77.0
